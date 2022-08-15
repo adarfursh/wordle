c6c9ebec19d1bae5f9d05fdfc4f963cb1314359f
Adar Furshpan <adar.f@gmail.com>
2022-08-15 23:03:07 +0300

    Updates

--- src/components/Letter.jsx
+++
@@ -1,9 +0,0 @@
-import { React } from "react";
-import "./letter.css";
-
-const Letter = (props) => {
-
-  return <div className={`square ${props.status}`}>{props.value}</div>;
-};
-
-export default Letter;
